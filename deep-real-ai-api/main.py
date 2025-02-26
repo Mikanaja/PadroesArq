929bc7be5323e4980368fbc7cbe63e0aa2b3142a
mentejoao <skhulzxd@gmail.com>
2025-02-26 09:49:33 -0300

    feat: adding try-except block to initialize s3 client

--- deep-real-ai-api/main.py
+++ deep-real-ai-api/main.py
@@ -1,23 +1,36 @@
 import boto3
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 from controllers.video_controller_s3 import get_router
 from transformers import ViTForImageClassification, ViTImageProcessor
 
 load_dotenv()
 
-s3_client = boto3.client(
-    "s3",
-    region_name=os.getenv("AWS_S3_REGION"),
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-)
+def initialize_s3_client():
+    try:
+        s3_client = boto3.client(
+            "s3",
+            region_name=os.getenv("AWS_S3_REGION"),
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        )
+        s3_client.list_buckets()  
+        return s3_client
+    except boto3.exceptions.S3UploadFailedError as e:
+        raise HTTPException(status_code=500, detail=f"Falha ao conectar ao S3: {str(e)}")
+    except boto3.exceptions.NoCredentialsError:
+        raise HTTPException(status_code=500, detail="Credenciais do AWS S3 não encontradas.")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erro inesperado ao conectar ao S3: {str(e)}")
+
+s3_client = initialize_s3_client()
 
 model = ViTForImageClassification.from_pretrained("prithivMLmods/Deep-Fake-Detector-v2-Model")
 image_processor = ViTImageProcessor.from_pretrained("prithivMLmods/Deep-Fake-Detector-v2-Model")
 
 app = FastAPI()
+
 app.include_router(get_router(image_processor, model, s3_client))
 
 @app.get("/")
