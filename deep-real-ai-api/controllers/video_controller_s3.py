c916dbb0e69096e360f79c7afa94550d9ba75c6c
mentejoao <skhulzxd@gmail.com>
2025-02-26 09:50:44 -0300

    feat: saving middle frame to debug + changing query param

--- deep-real-ai-api/controllers/video_controller_s3.py
+++ deep-real-ai-api/controllers/video_controller_s3.py
@@ -1,26 +1,26 @@
 from fastapi import APIRouter, HTTPException
-import ffmpeg
+from pydantic import BaseModel
+import os
 import cv2
 import numpy as np
 import torch
 from PIL import Image
 import requests
 from transformers import ViTForImageClassification, ViTImageProcessor
-from models.video_model import VideoModel
 import io
+
+class VideoUrlRequest(BaseModel):
+    video_url: str
 
 def get_router(image_processor, model, s3_client):
     router = APIRouter()
 
-    def extract_middle_frame(video_bytes):
-        """Extrai o frame do meio do vídeo (agora diretamente do buffer)."""
-        video_stream = io.BytesIO(video_bytes)
+    def extract_middle_frame(video_bytes, video_name):
+        """Extrai o frame do meio do vídeo e salva a imagem."""
+        video_array = np.frombuffer(video_bytes, dtype=np.uint8)
+        cap = cv2.VideoCapture()
+        cap.open(io.BytesIO(video_array))  # abre diretamente do buffer
 
-        # salva o vídeo temporariamente no buffer
-        with open("/tmp/temp_video.mp4", "wb") as temp_file:
-            temp_file.write(video_stream.read())
-
-        cap = cv2.VideoCapture("/tmp/temp_video.mp4")
         frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
         if frame_count == 0:
@@ -36,51 +36,51 @@ def get_router(image_processor, model, s3_client):
             raise ValueError("Não foi possível ler o frame do meio.")
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        return Image.fromarray(frame)
+        image = Image.fromarray(frame)
+
+        save_dir = "../middle_frame"
+        os.makedirs(save_dir, exist_ok=True)
+
+        image_path = os.path.join(save_dir, f"{video_name}_middle_frame.jpg")
+
+        try:
+            image.save(image_path)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Erro ao salvar a imagem: {str(e)}")
+
+        return image
 
     @router.post("/upload/")
-    async def upload_video(video_url: str):
+    async def upload_video(request: VideoUrlRequest):  # not a query param
         """Recebe a URL do vídeo, baixa e processa."""
         try:
+            video_url = request.video_url  
             response = requests.get(video_url, stream=True)
+
+            print(response)
 
             if response.status_code != 200:
                 raise HTTPException(status_code=400, detail="Falha ao baixar o vídeo.")
 
             video_bytes = response.content
+            video_name = video_url.split("/")[-1].split(".")[0]
 
-            probe = ffmpeg.probe(
-                io.BytesIO(video_bytes),
-                v="error",
-                select_streams="v:0",
-                show_entries="stream=duration",
-            )
-            duration = float(probe["streams"][0]["duration"])
+            middle_frame = extract_middle_frame(video_bytes, video_name)
 
-            middle_frame = extract_middle_frame(video_bytes)
+            try:
+                inputs = image_processor(images=middle_frame, return_tensors="pt")
 
-            inputs = image_processor(images=middle_frame, return_tensors="pt")
+                with torch.no_grad():
+                    outputs = model(**inputs)
+                    logits = outputs.logits
+                    predicted_class = torch.argmax(logits, dim=1).item()
 
-            with torch.no_grad():
-                outputs = model(**inputs)
-                logits = outputs.logits
-                predicted_class = torch.argmax(logits, dim=1).item()
+                label = model.config.id2label[predicted_class]
 
-            label = model.config.id2label[predicted_class]
+            except Exception as e:
+                raise HTTPException(status_code=500, detail=f"Erro ao gerar a classificação: {str(e)}")
 
-            video_data = VideoModel(
-                filename=video_url.split("/")[-1], 
-                content_type="video/mp4",
-                size=len(video_bytes),
-                duration=int(duration),
-                format_type="mp4",
-            )
-
-            return {
-                "message": "Upload processado com sucesso!",
-                "video_info": video_data,
-                "classification": label,
-            }
+            return {"classification": label}
 
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Erro ao processar o vídeo: {str(e)}")
